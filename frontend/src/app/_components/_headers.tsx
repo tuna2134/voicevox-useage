0797194fb30683e8b2e222d65cfd8e4cb2a4e8bc
mc_fdc <mc-fdc@tuna2134.jp>
2023-05-06 23:20:33 +0900

    Update _headers.tsx

--- frontend/src/app/_components/_headers.tsx
+++ frontend/src/app/_components/_headers.tsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
+
 const Header = () => {
+    const navLinks = [
+        {
+            name: "Official",
+            link: "https://voicevox.hiroshiba.jp",
+        }
+    ]
     return (
-        <header></header>
+        <header className="flex justify-between">
+            <div>
+                <h1 className="text-4xl">VOICEAGE</h1>
+            </div>
+            <nav>
+                {navLinks.map((navlink, index) => {
+                    return (
+                        <Link className="text-emerald-400" href={navLink.link}>navLink.name</Link>
+                    );
+                })}
+            </nav>
+        </header>
     )
 }
+
+export default Header;
